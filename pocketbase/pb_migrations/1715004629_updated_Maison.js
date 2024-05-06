26168e7adca72e1f325134bade6856fbae502404
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2024-05-06 16:10:55 +0200

    images --> image

---
+++ pocketbase/pb_migrations/1715004629_updated_Maison.js
@@ -0,0 +1,48 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("abmiqss6t9fk0y5")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "v53wesxu",
+    "name": "image",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 99,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("abmiqss6t9fk0y5")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "v53wesxu",
+    "name": "images",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 99,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
